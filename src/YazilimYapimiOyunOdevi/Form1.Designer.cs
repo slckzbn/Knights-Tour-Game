ef6c6bf28715233c68990b77c3d58371b6542f35
slckzbn <ozbenselcuk@gmail.com>
2019-03-06 01:48:58 +0300

    Update

---
+++ src/YazilimYapimiOyunOdevi/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace YazilimYapimiOyunOdevi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnYeni = new System.Windows.Forms.Button();
+            this.lblHamleSayisi = new System.Windows.Forms.Label();
+            this.lblDurum = new System.Windows.Forms.Label();
+            this.rd9x9 = new System.Windows.Forms.RadioButton();
+            this.rd8x8 = new System.Windows.Forms.RadioButton();
+            this.rd7x7 = new System.Windows.Forms.RadioButton();
+            this.rd6x6 = new System.Windows.Forms.RadioButton();
+            this.rd5x5 = new System.Windows.Forms.RadioButton();
+            this.lblBoyut = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnYeni
+            // 
+            this.btnYeni.Location = new System.Drawing.Point(32, 13);
+            this.btnYeni.Name = "btnYeni";
+            this.btnYeni.Size = new System.Drawing.Size(75, 23);
+            this.btnYeni.TabIndex = 0;
+            this.btnYeni.Text = "Yeni Oyun";
+            this.btnYeni.UseVisualStyleBackColor = true;
+            this.btnYeni.Click += new System.EventHandler(this.btnYeni_Click);
+            // 
+            // lblHamleSayisi
+            // 
+            this.lblHamleSayisi.AutoSize = true;
+            this.lblHamleSayisi.Location = new System.Drawing.Point(143, 18);
+            this.lblHamleSayisi.Name = "lblHamleSayisi";
+            this.lblHamleSayisi.Size = new System.Drawing.Size(82, 13);
+            this.lblHamleSayisi.TabIndex = 1;
+            this.lblHamleSayisi.Text = "Hamle Sayısı : 0";
+            // 
+            // lblDurum
+            // 
+            this.lblDurum.AutoSize = true;
+            this.lblDurum.Location = new System.Drawing.Point(32, 350);
+            this.lblDurum.Name = "lblDurum";
+            this.lblDurum.Size = new System.Drawing.Size(0, 13);
+            this.lblDurum.TabIndex = 2;
+            // 
+            // rd9x9
+            // 
+            this.rd9x9.AutoSize = true;
+            this.rd9x9.Location = new System.Drawing.Point(372, 140);
+            this.rd9x9.Name = "rd9x9";
+            this.rd9x9.Size = new System.Drawing.Size(42, 17);
+            this.rd9x9.TabIndex = 12;
+            this.rd9x9.Text = "9x9";
+            this.rd9x9.UseVisualStyleBackColor = true;
+            this.rd9x9.CheckedChanged += new System.EventHandler(this.rd9x9_CheckedChanged);
+            // 
+            // rd8x8
+            // 
+            this.rd8x8.AutoSize = true;
+            this.rd8x8.Location = new System.Drawing.Point(372, 117);
+            this.rd8x8.Name = "rd8x8";
+            this.rd8x8.Size = new System.Drawing.Size(42, 17);
+            this.rd8x8.TabIndex = 13;
+            this.rd8x8.Text = "8x8";
+            this.rd8x8.UseVisualStyleBackColor = true;
+            this.rd8x8.CheckedChanged += new System.EventHandler(this.rd8x8_CheckedChanged);
+            // 
+            // rd7x7
+            // 
+            this.rd7x7.AutoSize = true;
+            this.rd7x7.Location = new System.Drawing.Point(372, 94);
+            this.rd7x7.Name = "rd7x7";
+            this.rd7x7.Size = new System.Drawing.Size(42, 17);
+            this.rd7x7.TabIndex = 14;
+            this.rd7x7.Text = "7x7";
+            this.rd7x7.UseVisualStyleBackColor = true;
+            this.rd7x7.CheckedChanged += new System.EventHandler(this.rd7x7_CheckedChanged);
+            // 
+            // rd6x6
+            // 
+            this.rd6x6.AutoSize = true;
+            this.rd6x6.Location = new System.Drawing.Point(372, 71);
+            this.rd6x6.Name = "rd6x6";
+            this.rd6x6.Size = new System.Drawing.Size(42, 17);
+            this.rd6x6.TabIndex = 15;
+            this.rd6x6.Text = "6x6";
+            this.rd6x6.UseVisualStyleBackColor = true;
+            this.rd6x6.CheckedChanged += new System.EventHandler(this.rd6x6_CheckedChanged);
+            // 
+            // rd5x5
+            // 
+            this.rd5x5.AutoSize = true;
+            this.rd5x5.Checked = true;
+            this.rd5x5.Location = new System.Drawing.Point(372, 48);
+            this.rd5x5.Name = "rd5x5";
+            this.rd5x5.Size = new System.Drawing.Size(42, 17);
+            this.rd5x5.TabIndex = 16;
+            this.rd5x5.TabStop = true;
+            this.rd5x5.Text = "5x5";
+            this.rd5x5.UseVisualStyleBackColor = true;
+            this.rd5x5.CheckedChanged += new System.EventHandler(this.rd5x5_CheckedChanged_1);
+            // 
+            // lblBoyut
+            // 
+            this.lblBoyut.AutoSize = true;
+            this.lblBoyut.Location = new System.Drawing.Point(369, 18);
+            this.lblBoyut.Name = "lblBoyut";
+            this.lblBoyut.Size = new System.Drawing.Size(103, 13);
+            this.lblBoyut.TabIndex = 11;
+            this.lblBoyut.Text = "Oyun Alanını Seçiniz";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(501, 395);
+            this.Controls.Add(this.rd9x9);
+            this.Controls.Add(this.rd8x8);
+            this.Controls.Add(this.rd7x7);
+            this.Controls.Add(this.rd6x6);
+            this.Controls.Add(this.rd5x5);
+            this.Controls.Add(this.lblBoyut);
+            this.Controls.Add(this.lblDurum);
+            this.Controls.Add(this.lblHamleSayisi);
+            this.Controls.Add(this.btnYeni);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "At Oyunu";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnYeni;
+        private System.Windows.Forms.Label lblHamleSayisi;
+        private System.Windows.Forms.Label lblDurum;
+        private System.Windows.Forms.RadioButton rd9x9;
+        private System.Windows.Forms.RadioButton rd8x8;
+        private System.Windows.Forms.RadioButton rd7x7;
+        private System.Windows.Forms.RadioButton rd6x6;
+        private System.Windows.Forms.RadioButton rd5x5;
+        private System.Windows.Forms.Label lblBoyut;
+    }
+}
+
---
+++ src/YazilimYapimiOyunOdevi/Form1.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YazilimYapimiOyunOdevi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int HamleSayisi = 0;
+        int AlanBoyutu = 5;
+        PictureBox[,] zemin = new PictureBox[9, 9];
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            AlanOlustur(AlanBoyutu, 1);
+        }
+        private void btnYeni_Click(object sender, EventArgs e)
+        {
+            HamleSayisi = 0;
+            lblHamleSayisi.Text = "Hamle Sayısı : " + HamleSayisi.ToString();
+            lblDurum.Text = "";
+            AlanOlustur(AlanBoyutu, 0);
+            AlanOlustur(AlanBoyutu, 1);
+        }
+        public void AlanOlustur(int N, int kontrol)
+        {
+            int xpos = 32, ypos = 50;
+
+            if (kontrol == 1)
+            {
+                for (int x = 0; x < N; x++)
+                {
+                    for (int y = 0; y < N; y++)
+                    {
+                        zemin[x, y] = new PictureBox();
+                        zemin[x, y].Name = x.ToString() + y.ToString();
+                        zemin[x, y].Width = 30;
+                        zemin[x, y].Height = 30;
+                        zemin[x, y].Location = new Point(xpos, ypos);
+                        zemin[x, y].SizeMode = PictureBoxSizeMode.Zoom;
+                        zemin[x, y].BackColor = ColorTranslator.FromHtml("#0004ff");
+                        xpos += 32;
+                        this.Controls.Add(zemin[x, y]);
+                        zemin[x, y].Click += new EventHandler(zemin_Click);
+                    }
+                    xpos = 32;
+                    ypos += 32;
+                }
+            }
+            if (kontrol == 0)
+            {
+                for (int x = 0; x < 9; x++)
+                {
+                    for (int y = 0; y < 9; y++)
+                    {
+                        this.Controls.Remove(zemin[x, y]);
+                    }
+                }
+            }
+        }
+        public void zemin_Click(object sender, EventArgs e)
+        {
+            int x = 0, y = 0;
+            PictureBox sec = sender as PictureBox;
+            x = (Convert.ToInt32(sec.Name) - (Convert.ToInt32(sec.Name) % 10)) / 10;
+            y = Convert.ToInt32(sec.Name) % 10;
+            if (HamleSayisi == 0)
+            {
+                sec.BackColor = ColorTranslator.FromHtml("#ffffff");//beyaz yap
+                HamleSayisi++;
+                HamleAlani(x, y);
+            }
+            else
+            {
+                if (sec.BackColor == ColorTranslator.FromHtml("#11fc3c"))//yeşilse
+                {
+                    sec.BackColor = ColorTranslator.FromHtml("#ffffff");//beyaz yap
+                    HamleAlani(x, y);
+                    HamleSayisi++;
+                }
+            }
+            lblHamleSayisi.Text = "Hamle Sayısı : " + HamleSayisi.ToString();
+            if (HamleSayisi == (AlanBoyutu*AlanBoyutu))
+                lblDurum.Text = "Tebrikler Kazandınız..";
+        }
+        public void HamleAlani(int x, int y)
+        {
+            int k = 1;
+            Hamle_Devami();
+            if (!(x - 2 < 0) && !(y - 1 < 0))
+            {
+                if (zemin[x - 2, y - 1].BackColor != ColorTranslator.FromHtml("#ffffff"))
+                {
+                    zemin[x - 2, y - 1].BackColor = ColorTranslator.FromHtml("#11fc3c");
+                    k = 0;
+                }
+            }
+            if (!(x - 2 < 0) && !(y + 1 > AlanBoyutu - 1))
+            {
+                if (zemin[x - 2, y + 1].BackColor != ColorTranslator.FromHtml("#ffffff"))
+                {
+                    zemin[x - 2, y + 1].BackColor = ColorTranslator.FromHtml("#11fc3c");
+                    k = 0;
+                }
+            }
+            if (!(x - 1 < 0) && !(y - 2 < 0))
+            {
+                if (zemin[x - 1, y - 2].BackColor != ColorTranslator.FromHtml("#ffffff"))
+                {
+                    zemin[x - 1, y - 2].BackColor = ColorTranslator.FromHtml("#11fc3c");
+                    k = 0;
+                }
+            }
+            if (!(x - 1 < 0) && !(y + 2 > AlanBoyutu - 1))
+            {
+                if (zemin[x - 1, y + 2].BackColor != ColorTranslator.FromHtml("#ffffff"))
+                {
+                    zemin[x - 1, y + 2].BackColor = ColorTranslator.FromHtml("#11fc3c");
+                    k = 0;
+                }
+            }
+            if (!(x + 1 > AlanBoyutu - 1) && !(y - 2 < 0))
+            {
+                if (zemin[x + 1, y - 2].BackColor != ColorTranslator.FromHtml("#ffffff"))
+                {
+                    zemin[x + 1, y - 2].BackColor = ColorTranslator.FromHtml("#11fc3c");
+                    k = 0;
+                }
+            }
+            if (!(x + 1 > AlanBoyutu - 1) && !(y + 2 > AlanBoyutu - 1))
+            {
+                if (zemin[x + 1, y + 2].BackColor != ColorTranslator.FromHtml("#ffffff"))
+                {
+                    zemin[x + 1, y + 2].BackColor = ColorTranslator.FromHtml("#11fc3c");
+                    k = 0;
+                }
+            }
+            if (!(x + 2 > AlanBoyutu - 1) && !(y - 1 < 0))
+            {
+                if (zemin[x + 2, y - 1].BackColor != ColorTranslator.FromHtml("#ffffff"))
+                {
+                    zemin[x + 2, y - 1].BackColor = ColorTranslator.FromHtml("#11fc3c");
+                    k = 0;
+                }
+            }
+            if (!(x + 2 > AlanBoyutu - 1) && !(y + 1 > AlanBoyutu - 1))
+            {
+                if (zemin[x + 2, y + 1].BackColor != ColorTranslator.FromHtml("#ffffff"))
+                {
+                    zemin[x + 2, y + 1].BackColor = ColorTranslator.FromHtml("#11fc3c");
+                    k = 0;
+                }
+            }
+            if (k == 1)
+            {
+                lblDurum.Text = "Kaybettiniz..";
+            }
+        }
+        public void Hamle_Devami()
+        {
+            for (int x = 0; x < AlanBoyutu; x++)
+            {
+                for (int y = 0; y < AlanBoyutu; y++)
+                {
+                    if (zemin[x, y].BackColor == ColorTranslator.FromHtml("#11fc3c"))
+                    {
+                        zemin[x, y].BackColor = ColorTranslator.FromHtml("#0004ff");
+                    }
+                }
+            }
+        }
+
+        private void rd5x5_CheckedChanged_1(object sender, EventArgs e)
+        {
+            AlanBoyutu = 5;
+            HamleSayisi = 0;
+            lblHamleSayisi.Text = "Hamle Sayısı : " + HamleSayisi.ToString();
+            lblDurum.Text = "";
+            AlanOlustur(AlanBoyutu, 0);
+            AlanOlustur(AlanBoyutu, 1);
+        }
+
+        private void rd6x6_CheckedChanged(object sender, EventArgs e)
+        {
+            AlanBoyutu = 6;
+            HamleSayisi = 0;
+            lblHamleSayisi.Text = "Hamle Sayısı : " + HamleSayisi.ToString();
+            lblDurum.Text = "";
+            AlanOlustur(AlanBoyutu, 0);
+            AlanOlustur(AlanBoyutu, 1);
+        }
+
+        private void rd7x7_CheckedChanged(object sender, EventArgs e)
+        {
+            AlanBoyutu = 7;
+            HamleSayisi = 0;
+            lblHamleSayisi.Text = "Hamle Sayısı : " + HamleSayisi.ToString();
+            lblDurum.Text = "";
+            AlanOlustur(AlanBoyutu, 0);
+            AlanOlustur(AlanBoyutu, 1);
+        }
+
+        private void rd8x8_CheckedChanged(object sender, EventArgs e)
+        {
+            AlanBoyutu = 8;
+            HamleSayisi = 0;
+            lblHamleSayisi.Text = "Hamle Sayısı : " + HamleSayisi.ToString();
+            lblDurum.Text = "";
+            AlanOlustur(AlanBoyutu, 0);
+            AlanOlustur(AlanBoyutu, 1);
+        }
+
+        private void rd9x9_CheckedChanged(object sender, EventArgs e)
+        {
+            AlanBoyutu = 9;
+            HamleSayisi = 0;
+            lblHamleSayisi.Text = "Hamle Sayısı : " + HamleSayisi.ToString();
+            lblDurum.Text = "";
+            AlanOlustur(AlanBoyutu, 0);
+            AlanOlustur(AlanBoyutu, 1);
+        }
+    }
+}
